85542dfab24b96dcd426f132d28522810e3b20c2
Diana Mora <morra.dianna@gmail.com>
2021-05-18 03:42:43 -0400

    added image field

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show] #we get this method from devise
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = current_user.products.build
+  end
+
+  def edit
+  end
+
+  def create
+    @product = current_user.products.build(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: "Product was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+    end
+  end
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :description, :rating, :image)
+    end
+end
