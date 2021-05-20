85036404dc8812727bed967b8fdc182e44eb7adb
Diana Mora <morra.dianna@gmail.com>
2021-05-20 18:15:09 -0400

    added favicon icon to tab and updated index page welcome

--- db/schema.rb
+++ db/schema.rb
@@ -43,14 +43,6 @@ ActiveRecord::Schema.define(version: 2021_05_18_071849) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "breweries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "yelp_id"
-    t.string "city"
-    t.string "state"
-  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
